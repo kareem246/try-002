2674af50d3eb459a4ae6f45adb34e1984ddb7523
kareem246 <kokylightning@gmail.com>
2021-01-23 23:32:40 +0200

    try-php

---
+++ nav.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<nav style="text-align: center;">
+    <span>Home</span>
+    <span>Products</span>
+    <span>About</span>
+    <span>Blog</span>
+    <span>Contact Us</span>
+</nav>
